d90e371be6934115361e5d433bde8588a99428e4
Brent Snook <brent@fuglylogic.com>
2009-02-06 00:27:03 +1100

    added config command line argument when running servant

--- features/steps/servant.rb
+++ features/steps/servant.rb
@@ -1,6 +1,7 @@
 Given /^that a servant is listening for orders$/ do
   bin_directory = File.dirname(__FILE__) + '/../../bin'
+  feature_config = File.dirname(__FILE__) + '/../../bin'
 
-  ScenarioProcess.run File.join(bin_directory, 'baldrick')
+  ScenarioProcess.run "#{File.join(bin_directory, 'baldrick')} -c #{feature_config}"
   # TODO: make sure the servant has started up properly
 end
