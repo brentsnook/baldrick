597b70183195cedd1df2656386f4a941014f47bf
Brent Snook <brent@fuglylogic.com>
2009-10-14 01:12:02 +0100

    remove cuppa.rb, update manifest

--- cuppa.rb
+++
@@ -1,9 +0,0 @@
-#cuppa.rb
-require 'rubygems'
-require 'baldrick_serve'
-
-listen_to :feed, :at => 'http://search.twitter.com/search.atom?q=cup+of'
-
-on_hearing /cup of (.*?)[\.,]/ do |beverage, order|
-  puts "#{order[:who]} would like a cup of #{beverage}"
-end
